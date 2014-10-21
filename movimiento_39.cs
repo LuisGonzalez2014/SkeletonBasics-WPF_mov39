7df2a77e86e4a78d989e989f7458b69758bdcd5b
Luis Alejandro <l.alejandro.gb@gmail.com>
2014-10-21 16:35:33 +0200

    Nueva caja de texto en el ejecutable
    Se ha añadido un cuadro de texto que servirá para depurar el programa o
    ir comprobando la ejecución del mismo

---
+++ movimiento_39.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Samples.Kinect.SkeletonBasics
+{
+   using System.IO;
+   using System.Windows;
+   using System.Windows.Media;
+   using Microsoft.Kinect;
+
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+   }
+}
